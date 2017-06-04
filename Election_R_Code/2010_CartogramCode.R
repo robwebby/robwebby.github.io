dd0cf76ba479effae5c686adb2639027b7b97565
robwebby <rob.a.webster1@gmail.com>
2017-06-04 23:12:52 +0100

    Cartograms

---
+++ Election_R_Code/2010_CartogramCode.R
@@ -0,0 +1,35 @@
+setwd("/Users/robwebster/Documents/GitHub/robwebby.github.io")
+
+Constituencies <- readOGR("Cartogram_GE.shp")
+
+GE_2010 <- read.csv("GE2010_Results.csv")
+
+GE2010_WGS <- merge(Constituencies,GE_2010, by = "CODE")
+
+Elec10pal <- colorFactor(c("darkblue","chartreuse","firebrick2","goldenrod3","dimgrey","darkgreen","gold","darkorchid1"), GE2015_WGS$Winner.15)
+
+labelelec2010 <- sprintf(
+  "<strong>%s</strong><br/>Winner: <strong>%s</strong><br/> Lab Vote Share <strong> %g   </strong> <br /> Con Vote Share <strong> %g  </strong> <br />Lib Dem Vote Share <strong> %g  </strong> <br />UKIP Vote Share <strong> %g  </strong> <br />SNP Vote Share <strong> %g  </strong> <br />Green Vote Share <strong> %g  </strong> <br />Other Vote Share <strong> %g  </strong> <br />", GE2010_WGS$Constituency.Name,GE2010_WGS$Winner.10,GE2010_WGS$Labour.Vote.Share.10,GE2010_WGS$Conservative.Vote.Share.10,GE2010_WGS$Lib.Dems.Vote.Share.10,GE2010_WGS$UKIP.Vote.Share.10,GE2010_WGS$SNP.Vote.Share.10,GE2010_WGS$Green.Vote.Share.10,GE2010_WGS$Other.Vote.Share.10
+) %>% lapply(htmltools::HTML)
+
+GE2010_Leaflet <- leaflet(GE2010_WGS) %>%
+  fitBounds(-10.02,49.67,2.09,58.06) %>% 
+  addPolygons(stroke = FALSE, smoothFactor = 0.2, fillOpacity = 1,
+              color = ~Elec10pal(Winner.10), 
+              highlight = highlightOptions(
+                weight = 5,
+                color = "#666",
+                dashArray = "",
+                fillOpacity = 0.7,
+                bringToFront = TRUE),
+              label = labelelec2010,
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "1px 1px"),
+                textsize = "9px",
+                direction = "auto", group = "Election 10")
+  ) %>% addLegend("bottomright", pal = Elec10pal, values = ~Winner.10,
+                  title = "Winner GE 2010",
+                  labFormat = labelFormat(prefix = ""),
+                  opacity = 1)
+
+saveWidget(GE2010_Leaflet,file = "Election_2010_Cartogram.html")
---
+++ Election_R_Code/2015_CartogramCode.R
@@ -0,0 +1,35 @@
+setwd("/Users/robwebster/Documents/GitHub/robwebby.github.io")
+
+Constituencies <- readOGR("Cartogram_GE.shp")
+
+GE_2015 <- read.csv("GE2015_Results.csv")
+
+GE2015_WGS <- merge(Constituencies,GE_2015, by = "CODE")
+
+Elec15pal <- colorFactor(c("darkblue","chartreuse","firebrick2","goldenrod3","dimgrey","darkgreen","gold","darkorchid1"), GE2015_WGS$Winner.15)
+
+labelelec2015 <- sprintf(
+  "<strong>%s</strong><br/>Winner: <strong>%s</strong><br/> Lab Vote Share <strong> %g  (%g) </strong> <br /> Con Vote Share <strong> %g (%g) </strong> <br />Lib Dem Vote Share <strong> %g  (%g)</strong> <br />UKIP Vote Share <strong> %g (%g) </strong> <br />SNP Vote Share <strong> %g (%g) </strong> <br />Green Vote Share <strong> %g (%g) </strong> <br />Other Vote Share <strong> %g (%g) </strong> <br />", GE2015_WGS$Constituency.Name,GE2015_WGS$Winner.15,GE2015_WGS$Labour.Vote.Share.15,GE2015_WGS$Labour.Vote.Share.Change.15,GE2015_WGS$Conservative.Vote.Share.15,GE2015_WGS$Conservative.Vote.Share.Change.15,GE2015_WGS$Lib.Dems.Vote.Share.15,GE2015_WGS$Lib.Dems.Vote.Share.Change.15,GE2015_WGS$UKIP.Vote.Share.15,GE2015_WGS$UKIP.Vote.Share.Change.15,GE2015_WGS$SNP.Vote.Share.15,GE2015_WGS$SNP.Vote.Share.Change.15,GE2015_WGS$Green.Vote.Share.15,GE2015_WGS$Green.Vote.Share.Change.15,GE2015_WGS$Other.Vote.Share.15,GE2015_WGS$Other.Vote.Share.Change.15
+) %>% lapply(htmltools::HTML)
+
+GE2015_Leaflet <- leaflet(GE2015_WGS) %>%
+  fitBounds(-10.02,49.67,2.09,58.06) %>% 
+  addPolygons(stroke = FALSE, smoothFactor = 0.2, fillOpacity = 1,
+              color = ~Elec15pal(Winner.15), 
+              highlight = highlightOptions(
+                weight = 5,
+                color = "#666",
+                dashArray = "",
+                fillOpacity = 0.7,
+                bringToFront = TRUE),
+              label = labelelec2015,
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "1px 1px"),
+                textsize = "9px",
+                direction = "auto", group = "Election 15")
+  ) %>% addLegend("bottomright", pal = Elec15pal, values = ~Winner.15,
+                  title = "Winner GE 2015",
+                  labFormat = labelFormat(prefix = ""),
+                  opacity = 1)
+
+saveWidget(GE2015_Leaflet,file = "Election_2015_Cartogram.html")
